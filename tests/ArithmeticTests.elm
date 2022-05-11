085d39ce45f2d9803ade6ab807d9ea2aeedfcc86
edkelly303 <edwardjkelly@gmail.com>
2022-05-11 12:10:40 +0100

    Update ArithmeticTests.elm

--- tests/ArithmeticTests.elm
+++ tests/ArithmeticTests.elm
@@ -22,18 +22,34 @@ suite =
     describe ".op"
         [ test "Add" <|
             \_ ->
-                arith.op (A 6) add (A 2)
+                arith (A 6) add (A 2)
                     |> Expect.equal (A 8)
         , test "Sub" <|
             \_ ->
-                arith.op (A 6) sub (A 2)
+                arith (A 6) sub (A 2)
                     |> Expect.equal (A 4)
         , test "Mul" <|
             \_ ->
-                arith.op (A 6) mul (A 2)
+                arith (A 6) mul (A 2)
                     |> Expect.equal (A 12)
         , test "Div" <|
             \_ ->
-                arith.op (A 6) div (A 2)
+                arith (A 6) div (A 2)
                     |> Expect.equal (A 3)
+        , test "Div0" <|
+            \_ ->
+                arith (A 6) div0 (A 2)
+                    |> Expect.equal (A 3)
+        , test "Div0 of 0" <|
+            \_ ->
+                arith (A 1) div0 (A 0)
+                    |> Expect.equal (A 0)
+        , test "DivM" <|
+            \_ ->
+                arith (A 6) divM (A 2)
+                    |> Expect.equal (A 3)
+        , test "DivM of 0" <|
+            \_ ->
+                arith (A 1) divM (A 0)
+                    |> Expect.equal (A -2147483648)
         ]
